fix the magnetic forms derivatives, upgrade the em evolution function

em_fields/RF_nonuniform_Bz.py
@@ -4,7 +4,7 @@ from mpl_toolkits.mplot3d import Axes3D
 
 from em_fields.default_settings import define_default_settings
 from em_fields.em_functions import evolve_particle_in_em_fields, get_thermal_velocity, get_cyclotron_angular_frequency
-from em_fields.magnetic_forms import magnetic_field_jaeger
+from em_fields.magnetic_forms import magnetic_field_post
 
 plt.rcParams.update({'font.size': 14})
 plt.rcParams.update({'axes.labelpad': 15})
@@ -25,6 +25,10 @@ kB_eV = settings['kB_eV']
 v_th = get_thermal_velocity(T_eV, m, kB_eV)
 
 Rm = 3.0
+print('Rm = ' + str(Rm))
+
+loss_cone_angle = np.arcsin(Rm ** (-0.5)) * 360 / (2 * np.pi)
+print('loss_cone_angle = ' + str(loss_cone_angle))
 
 # c = np.inf
 c = 3e8
@@ -36,32 +40,59 @@ c = 3e8
 print('v_th / c = ', v_th / c)
 
 # B0 = 0 # Tesla
+# B0 = 0.1  # Tesla
+# B0 = 0.2  # Tesla
 B0 = 1.0  # Tesla
 # B0 = -1.0 # Tesla
 
 omega_cyclotron = get_cyclotron_angular_frequency(q, B0, m)
+# omega_cyclotron = get_cyclotron_angular_frequency(q, B0 * Rm, m)
+# omega_cyclotron = get_cyclotron_angular_frequency(q, B0 * Rm * 0.5, m)
 tau_cyclotron = 2 * np.pi / omega_cyclotron
 
-# v_0 = v_th * np.array([0, -1, 0.5])
-v_0 = np.array([0, 1, 10])
+angle_to_z_axis = 0  # deg
+# angle_to_z_axis = 0.5 * loss_cone_angle # deg
+# angle_to_z_axis = 0.95 * loss_cone_angle # deg
+# angle_to_z_axis = 0.99 * loss_cone_angle # deg
+# angle_to_z_axis = loss_cone_angle # deg
+# angle_to_z_axis = 1.01 * loss_cone_angle # deg
+# angle_to_z_axis = 1.10 * loss_cone_angle # deg
+print('angle_to_z_axis = ' + str(angle_to_z_axis) + ' degrees')
+
+angle_to_z_axis_rad = angle_to_z_axis / 360 * 2 * np.pi
+v_0 = np.array([0, np.sin(angle_to_z_axis_rad), np.cos(angle_to_z_axis_rad)])
+
+# v_0 = np.array([0, 1, 10])
 # v_0 = np.array([0, 10, 1])
+# v_0 = np.array([0, 2, 1])
 # v_0 = np.array([0, 1, 1])
-# v_0 /= np.linalg.norm(v_0)
+# v_0 = np.array([0, 0.5, 1])
+
+# normalize the velocity vector
 v_0 = v_0 / np.linalg.norm(v_0)
 v_0 *= v_th
+v_0_norm = np.linalg.norm(v_0)
 
 cyclotron_radius = np.linalg.norm(v_0) / omega_cyclotron
 # x_0 = np.array([0, 0, 0])
-x_0 = cyclotron_radius * np.array([1, 0, 0])
+# x_0 = cyclotron_radius * np.array([1, 0, 0])
+# x_0 = cyclotron_radius * np.array([0, 0, 0])
+# x_0 = np.array([0, 0, 0])
+# x_0 = np.array([0, 0, 0.5])
+x_0 = np.array([0, 0, 0])
 
 l = 1.0  # m (interaction length)
+# l = 2.0  # m (interaction length)
 # l = 100.0  # m (interaction length)
 v_z = v_0[2]
-t_max = l / v_z
+# t_max = l / v_z
+# t_max = 2 * l / v_z
+t_max = 10 * l / v_z
 # t_max = min(t_max, 100 * tau_cyclotron)
 
-dt = tau_cyclotron / 50 / Rm
+# dt = tau_cyclotron / 50 / Rm
 # dt = tau_cyclotron / 200
+dt = tau_cyclotron / 20
 # num_steps = 1000
 num_steps = int(t_max / dt)
 # num_steps = min(num_steps, 3000)
@@ -74,10 +105,16 @@ if B0 == 0:  # pick a default
 else:
     anticlockwise = np.sign(B0)
 
-# E_RF = 0.0
-E_RF = 1  # kV/m
+E_RF = 0.0
+# E_RF = 1  # kV/m
+# E_RF = 3  # kV/m
 # E_RF = 5  # kV/m
 # E_RF = 10  # kV/m
+# E_RF = 15  # kV/m
+# E_RF = 20  # kV/m
+# E_RF = 50  # kV/m
+# E_RF = 75  # kV/m
+# E_RF = 80  # kV/m
 # E_RF = 100  # kV/m
 E_RF *= 1e3  # the SI units is V/m
 
@@ -113,23 +150,36 @@ def E_function(x, t):
 
 def B_function(x, t):
     use_transverse_fields = True
-    # B_axial = magnetic_field_constant(B0)
-    # B_axial = magnetic_field_logan(x, B0, Rm, l, use_transverse_fields=use_transverse_fields)
-    B_axial = magnetic_field_jaeger(x, B0, Rm, l, use_transverse_fields=use_transverse_fields)
-    # B_axial = magnetic_field_post(x, B0, Rm, l, use_transverse_fields=use_transverse_fields)
+    # use_transverse_fields = False
+
+    # B_mirror = magnetic_field_constant(B0)
+    # B_mirror = magnetic_field_logan(x, B0, Rm, l, use_transverse_fields=use_transverse_fields)
+    # B_mirror = magnetic_field_jaeger(x, B0, Rm, l, use_transverse_fields=use_transverse_fields)
+    B_mirror = magnetic_field_post(x, B0, Rm, l, use_transverse_fields=use_transverse_fields)
 
     # B_RF = 1/c * k_hat cross E_RF
     # https://en.wikipedia.org/wiki/Sinusoidal_plane-wave_solutions_of_the_electromagnetic_wave_equation
     # https://en.wikipedia.org/wiki/List_of_trigonometric_identities
+    z = x[2]
     B_RF = E_RF / c * np.array([-np.sign(k) * np.cos(k * z - omega * t + phase_RF),
                                 np.sign(k) * anticlockwise * np.sin(k * z - omega * t + phase_RF),
                                 0])
     # B_RF = 0 # test that does not satisfy Maxwell equations.
-    return B_axial + B_RF
+    return B_mirror + B_RF
 
 
-t, x, y, z, vx, vy, vz = evolve_particle_in_em_fields(x_0, v_0, dt, E_function, B_function,
-                                                      num_steps=num_steps, q=q, m=m)
+hist = evolve_particle_in_em_fields(x_0, v_0, dt, E_function, B_function,
+                                    num_steps=num_steps, q=q, m=m, return_fields=True)
+t = hist['t']
+x = hist['x'][:, 0]
+y = hist['x'][:, 1]
+z = hist['x'][:, 2]
+vx = hist['v'][:, 0]
+vy = hist['v'][:, 1]
+vz = hist['v'][:, 2]
+E = hist['E']
+B = hist['B']
+
 R = np.sqrt(x ** 2 + y ** 2)
 v_norm = np.sqrt(vx ** 2 + vy ** 2 + vz ** 2)
 
@@ -137,8 +187,10 @@ v_norm = np.sqrt(vx ** 2 + vy ** 2 + vz ** 2)
 t /= tau_cyclotron
 x /= cyclotron_radius
 y /= cyclotron_radius
-z /= cyclotron_radius
+# z /= cyclotron_radius
 R /= cyclotron_radius
+z /= l
+# R /= l
 vx /= v_th
 vy /= v_th
 vz /= v_th
@@ -149,20 +201,25 @@ linewidth = 2
 
 plt.figure(1)
 # plt.subplot(1,2,1)
-plt.plot(t, x, label='x', linewidth=linewidth, color='b')
-plt.plot(t, y, label='y', linewidth=linewidth, color='g')
-plt.plot(t, z, label='z', linewidth=linewidth, color='r')
-plt.plot(t, R, label='R', linewidth=linewidth, color='k')
+# plt.plot(t, x, label='x', linewidth=linewidth, color='b')
+# plt.plot(t, y, label='y', linewidth=linewidth, color='g')
+plt.plot(t, R, label='$R/r_{cyc}$', linewidth=linewidth, color='b')
+plt.plot(t, z, label='$z/l$', linewidth=linewidth, color='r')
 plt.legend()
 plt.xlabel('t / $\\tau_{cyc}$')
-plt.ylabel('coordinate / $r_{cyc}$')
+# plt.ylabel('coordinate / $r_{cyc}$')
+plt.ylabel('coordinate (normalized)')
 plt.grid(True)
 plt.tight_layout()
 
 plt.figure(4)
 # plt.subplot(1,2,2)
-plt.plot(t, vx, label='$v_x$', linewidth=linewidth, color='b')
-plt.plot(t, vy, label='$v_y$', linewidth=linewidth, color='g')
+# plt.plot(t, vx, label='$v_x$', linewidth=linewidth, color='b')
+# plt.plot(t, vy, label='$v_y$', linewidth=linewidth, color='g')
+v_r = np.sqrt(vx ** 2 + vy ** 2)
+plt.plot(t, v_r, label='$v_r$', linewidth=linewidth, color='b')
+v_perp_loss_cone = v_norm * Rm ** (-0.5)
+plt.plot(t, v_perp_loss_cone, label='$v_{r,LC}$', linewidth=linewidth, linestyle='--', color='c')
 plt.plot(t, vz, label='$v_z$', linewidth=linewidth, color='r')
 plt.plot(t, v_norm, label='$v_{norm}$', linewidth=linewidth, color='k')
 plt.legend()
@@ -180,32 +237,47 @@ plt.tight_layout()
 
 plt.figure(3)
 plt.plot(R, z, linewidth=linewidth)
-plt.xlabel('R / $r_{cyc}$')
-plt.ylabel('z / $r_{cyc}$')
+# plt.xlabel('R / $r_{cyc}$')
+# plt.ylabel('z / $r_{cyc}$')
+plt.xlabel('R (normalized)')
+plt.ylabel('z (normalized)')
 plt.grid(True)
 # plt.legend()
 plt.tight_layout()
 
 plt.figure(5)
-E = 0.5 * v_norm ** 2.0
+E_kin = 0.5 * v_norm ** 2.0
 # plt.plot(t, E / E[0], linewidth=linewidth)
-plt.plot(t, (E - E[0]) / E[0] * 100, linewidth=linewidth)
+plt.plot(t, (E_kin - E_kin[0]) / E_kin[0] * 100, linewidth=linewidth)
 plt.xlabel('t / $\\tau_{cyc}$')
 # plt.ylabel('$E / E_0$')
 plt.ylabel('kinetic energy % increase')
 plt.grid(True)
 plt.tight_layout()
+print('energy change: ' + str((E_kin[-1] - E_kin[0]) / E_kin[0] * 100) + '%')
 
-print('energy change: ' + str((E[-1] - E[0]) / E[0] * 100) + '%')
+plt.figure(6)
+# plt.plot(t, B[:, 0], label='$B_x$', linewidth=linewidth, color='b')
+# plt.plot(t, B[:, 1], label='$B_y$', linewidth=linewidth, color='g')
+plt.plot(t, B[:, 0], label='$B_r$', linewidth=linewidth, color='b')
+plt.plot(t, B[:, 2], label='$B_z$', linewidth=linewidth, color='r')
+plt.xlabel('t / $\\tau_{cyc}$')
+plt.ylabel('Tesla')
+plt.grid(True)
+plt.tight_layout()
+plt.legend()
 
 if plot3d_exists is True:
-    plt.figure(6)
+    plt.figure(7)
 else:
-    fig = plt.figure(6)
+    fig = plt.figure(7)
     ax = Axes3D(fig)
 ax.plot(x, y, z, linewidth=linewidth)
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
+# ax.set_xlabel('x')
+# ax.set_ylabel('y')
+# ax.set_zlabel('z')
+ax.set_xlabel('x (normalized)')
+ax.set_ylabel('y (normalized)')
+ax.set_zlabel('z (normalized)')
 ax.set_title('particle 3d trajectory')
 # plt.legend()

em_fields/em_functions.py
@@ -19,34 +19,38 @@ def get_cyclotron_angular_frequency(q, B, m):
 
 
 def evolve_particle_in_em_fields(x_0, v_0, dt, E_function, B_function, t_0=0, q=1.0, m=1.0,
-                                 stop_criterion='steps', num_steps=None, t_max=None):
+                                 stop_criterion='steps', num_steps=None, t_max=None, return_fields=False):
     """
     Advance a charged particle in time under the influence of E,B fields.
     """
     if stop_criterion == 'time':
         num_steps = t_max / dt
-    x_list = [x_0]
-    v_list = [v_0]
-    t_list = [t_0]
     t = t_0
-    for i in range(num_steps):
-        x_new, v_new = particle_integration_step(x_list[-1], v_list[-1], t_list[-1],
-                                                 dt, E_function, B_function, q=q, m=m)
-        x_list += [x_new]
-        v_list += [v_new]
-        t += dt
-        t_list += [t]
 
-    t = np.array(t_list)
-    x_list = np.array(x_list)
-    v_list = np.array(v_list)
-    x = x_list[:, 0]
-    y = x_list[:, 1]
-    z = x_list[:, 2]
-    vx = v_list[:, 0]
-    vy = v_list[:, 1]
-    vz = v_list[:, 2]
-    return t, x, y, z, vx, vy, vz
+    # define a dictionary that will collect all the particles history as it goes
+    hist = {}
+    hist['x'] = [x_0]
+    hist['v'] = [v_0]
+    hist['t'] = [t_0]
+    if return_fields is True:
+        hist['E'] = [E_function(x_0, t_0)]
+        hist['B'] = [B_function(x_0, t_0)]
+
+    for i in range(num_steps):
+        x_new, v_new = particle_integration_step(hist['x'][-1], hist['v'][-1], hist['t'][-1],
+                                                 dt, E_function, B_function, q=q, m=m)
+        hist['x'] += [x_new]
+        hist['v'] += [v_new]
+        t += dt
+        hist['t'] += [t]
+
+        if return_fields is True:
+            hist['E'] += [E_function(x_new, t)]
+            hist['B'] += [B_function(x_new, t)]
+
+    for key in hist:
+        hist[key] = np.array(hist[key])
+    return hist
 
 
 def particle_integration_step(x_0, v_0, t, dt, E_function, B_function, q=1.0, m=1.0):
@@ -72,4 +76,3 @@ def particle_integration_step(x_0, v_0, t, dt, E_function, B_function, q=1.0, m=
     v_new = v_plus + dt * q / m / 2.0 * E_half
     x_new = x_half + dt / 2.0 * v_new
     return x_new, v_new
-

em_fields/magnetic_forms.py
@@ -11,9 +11,11 @@ def get_transverse_magnetic_fields(x, dBz_dz):
     https://www.tcd.ie/Physics/people/Peter.Gallagher/lectures/PlasmaPhysics/Lecture5_single_particle.pdf
     """
     r = get_radius(x)
-    Br = -0.5 * r * dBz_dz
-    Bx = x[0] / r * Br
-    By = x[1] / r * Br
+    # Br = -0.5 * r * dBz_dz
+    # Bx = x[0] / r * Br
+    # By = x[1] / r * Br
+    Bx = -0.5 * x[0] * dBz_dz
+    By = -0.5 * x[1] * dBz_dz
     return Bx, By
 
 
@@ -28,7 +30,7 @@ def magnetic_field_logan(x, B0, Rm, l, use_transverse_fields=True):
     """
     z = x[2]
     Bz = B0 * (1 + (Rm - 1.0) * np.sin(np.pi * (z - l / 2.0) / l) ** 2.0)
-    dBz_dz = np.pi / l * B0 * (Rm - 1.0) * np.cos(np.pi * (z - l / 2.0) / l) \
+    dBz_dz = 2 * np.pi / l * B0 * (Rm - 1.0) * np.cos(np.pi * (z - l / 2.0) / l) \
              * np.sin(np.pi * (z - l / 2.0) / l)
     if use_transverse_fields:
         Bx, By = get_transverse_magnetic_fields(x, dBz_dz)
@@ -43,8 +45,7 @@ def magnetic_field_jaeger(x, B0, Rm, l, use_transverse_fields=True):
     """
     z = x[2]
     Bz = B0 * (1 + (Rm - 1) / 2.0 * (1 - np.cos(2 * np.pi * (z - l / 2) / l)))
-    dBz_dz = - 2 * np.pi / l * B0 * (Rm - 1.0) / Rm * np.sin(2 * np.pi * (z - l / 2) / l) \
-             * np.cos(2 * np.pi * (z - l / 2) / l)
+    dBz_dz = np.pi / l * B0 * (Rm - 1) * np.sin(2 * np.pi * (z - l / 2) / l)
     if use_transverse_fields:
         Bx, By = get_transverse_magnetic_fields(x, dBz_dz)
     else:
@@ -59,8 +60,8 @@ def magnetic_field_post(x, B0, Rm, l, use_transverse_fields=True):
     z = x[2]
     lamda = 5.5
     Bz = B0 * (1 + (Rm - 1.0) * np.exp(- lamda * np.sin(np.pi * z / l) ** 2.0))
-    dBz_dz = - lamda * np.pi / l * B0 * (Rm - 1.0) * np.cos(np.pi * (z - l / 2.0) / l) \
-             * np.sin(np.pi * (z - l / 2.0) / l) \
+    dBz_dz = - lamda * 2 * np.pi / l * B0 * (Rm - 1.0) * np.cos(np.pi * z / l) \
+             * np.sin(np.pi * z / l) \
              * np.exp(- lamda * np.sin(np.pi * z / l) ** 2.0)
     if use_transverse_fields:
         Bx, By = get_transverse_magnetic_fields(x, dBz_dz)

em_fields/particle_evolution_examples.py
@@ -14,8 +14,8 @@ plot3d_exists = False
 # plot3d_exists = True
 
 # example_system = 'helix'
-example_system = 'helix_with_RF'
-# example_system = '2d_static'
+# example_system = 'helix_with_RF'
+example_system = '2d_static'
 # example_system = 'tokamak_banana'
 # example_system = 'tokamak_transit'
 # example_system = 'magnetic_mirror'
@@ -189,7 +189,15 @@ elif example_system in ['magnetic_mirror']:
 else:
     raise ValueError('invalid example_system = ' + example_system)
 
-t, x, y, z, vx, vy, vz = evolve_particle_in_em_fields(x_0, v_0, dt, E_function, B_function, num_steps=num_steps)
+hist = evolve_particle_in_em_fields(x_0, v_0, dt, E_function, B_function, num_steps=num_steps)
+t = hist['t']
+x = hist['x'][:, 0]
+y = hist['x'][:, 1]
+z = hist['x'][:, 2]
+vx = hist['v'][:, 0]
+vy = hist['v'][:, 1]
+vz = hist['v'][:, 2]
+
 R = np.sqrt(x ** 2 + y ** 2)
 v_norm = np.sqrt(vx ** 2 + vy ** 2 + vz ** 2)
 

em_fields/plot_magnetic_forms.py
@@ -6,6 +6,7 @@ from em_fields.magnetic_forms import magnetic_field_logan, magnetic_field_jaeger
 plt.rcParams.update({'font.size': 14})
 # plt.rcParams.update({'font.size': 16})
 plt.rcParams.update({'axes.labelpad': 15})
+plt.rcParams.update({'lines.linewidth': 3})
 plt.rcParams.update({'lines.linestyle': '-'})
 # plt.rcParams.update({'lines.linestyle': '--'})
 # plt.rcParams.update({'lines.linestyle': ':'})
@@ -15,30 +16,53 @@ plt.close('all')
 l = 1
 # z = np.linspace(0, l, 1000)
 z = np.linspace(-l, l, 1000)
+# z = np.linspace(-2*l, 2*l, 1000)
 
 # mirror ratio
 Rm = 3
 # Rm = 7
 # Rm = 10
 
-x = np.array([z, z, z])
+# x = np.array([0*z, 0*z, z])
+x = np.array([0 * z + l / 2, 0 * z + l / 2, z])
 
 B = magnetic_field_logan(x, 1, Rm, l)
-B_logan = B[2]
+B_logan = B
 
 B = magnetic_field_jaeger(x, 1, Rm, l)
-B_jaeger = B[2]
+B_jaeger = B
 
 B = magnetic_field_post(x, 1, Rm, l)
-B_post = B[2]
+B_post = B
 
 # plot axial magnetic fields
 plt.figure(1)
-plt.plot(z, B_logan, label='Logan et al', color='b')
-plt.plot(z, B_post, label='Post', color='r')
-plt.plot(z, B_jaeger, label='Jaeger et al', color='g')
+plt.plot(z, B_logan[2], label='Logan et al', color='b')
+# plt.plot(z, B_jaeger[2], '--', label='Jaeger et al', color='g')
+plt.plot(z, B_post[2], label='Post', color='r')
 plt.legend()
 plt.xlabel('z [m]')
-plt.ylabel('B [T]')
+plt.ylabel('$B_z$ [T]')
+plt.grid(True)
+plt.tight_layout()
+
+# plot radial magnetic field
+plt.figure(2)
+plt.plot(z, B_logan[0], label='Logan et al', color='b')
+# plt.plot(z, B_jaeger[0], '--', label='Jaeger et al', color='g')
+plt.plot(z, B_post[0], label='Post', color='r')
+plt.legend()
+plt.xlabel('z [m]')
+plt.ylabel('$B_x$ [T]')
+plt.grid(True)
+plt.tight_layout()
+
+plt.figure(3)
+plt.plot(z, B_logan[1], label='Logan et al', color='b')
+# plt.plot(z, B_jaeger[1], '--', label='Jaeger et al', color='g')
+plt.plot(z, B_post[1], label='Post', color='r')
+plt.legend()
+plt.xlabel('z [m]')
+plt.ylabel('$B_y$ [T]')
 plt.grid(True)
 plt.tight_layout()