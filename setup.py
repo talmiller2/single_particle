9160fc44e41777c83a634994dda8c78c976d3e17
talmiller2 <talmiller@gmail.com>
2021-03-25 16:49:39 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="single_particle",
+    author="Tal Miller",
+    author_email="talmiller@gmail.com",
+    packages=['em_fields', 'loop_fields'],
+)
+
+# install locally using: python setup.py install --user
