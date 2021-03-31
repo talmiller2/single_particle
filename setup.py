3dd036ded3b31d71d1ed6661434452c8d2247357
talmiller2 <talmiller@gmail.com>
2021-03-31 10:47:08 +0300

    try again

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ setup(
     name="single_particle",
     author="Tal Miller",
     author_email="talmiller@gmail.com",
-    packages=['em_fields', 'loop_fields'],
+    # packages=['em_fields', 'loop_fields'],
+    packages=['em_fields'],
+    # scripts=['em_fields/evolution_slave.py'],
 )
 
 # install locally using: python setup.py install --user
+# another option: pip install . --user --upgrade
+# save exe permissions that git accepts: git update-index --chmod=+x file_name
