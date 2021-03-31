6ac5c397d7e5e786fd61947ee8853ac65ff59731
talmiller2 <talmiller@gmail.com>
2021-03-31 11:30:17 +0300

    try again

--- em_fields/slurm_functions.py
+++ em_fields/slurm_functions.py
@@ -1,9 +1,4 @@
-import os
-
-
-#
-# tmp = 5
-
 def get_script_evolution_slave():
-    script_path = os.path.dirname(os.path.abspath(__file__)) + '/evolution_slave.py'
+    # script_path = os.path.dirname(os.path.abspath(__file__)) + '/evolution_slave.py'
+    script_path = 'evolution_slave.py'  # TODO: test using scripts install, but long term dont want this behavior
     return script_path
--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
     name="single_particle",
     author="Tal Miller",
     author_email="talmiller@gmail.com",
-    # packages=['em_fields', 'loop_fields'],
-    packages=['em_fields'],
-    # scripts=['em_fields/evolution_slave.py'],
+    packages=['em_fields', 'loop_fields'],
+    scripts=['em_fields/evolution_slave.py'],
 )
 
 # install locally using: python setup.py install --user
 # another option: pip install . --user --upgrade
 # save exe permissions that git accepts: git update-index --chmod=+x file_name
+# ~/.local/lib/python3.6/site-packages/
