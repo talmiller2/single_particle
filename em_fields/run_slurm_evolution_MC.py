7d6634da746239b5c80609d5eadae8701cff2381
talmiller2 <talmiller@gmail.com>
2021-06-03 18:23:20 +0300

    adding a run with constant velocities

--- em_fields/run_slurm_evolution_MC.py
+++ em_fields/run_slurm_evolution_MC.py
@@ -13,7 +13,6 @@ from em_fields.default_settings import define_default_settings, define_default_f
 
 import numpy as np
 from scipy.io import savemat
-from scipy.stats import maxwell
 
 slurm_kwargs = {'partition': 'core'}  # default
 # slurm_kwargs = {'partition': 'socket'}
@@ -30,9 +29,9 @@ settings = {}
 settings = define_default_settings()
 
 field_dict = {}
-field_dict['E_RF_kVm'] = 0  # kV/m
+# field_dict['E_RF_kVm'] = 0  # kV/m
 # field_dict['E_RF_kVm'] = 2  # kV/m
-# field_dict['E_RF_kVm'] = 4  # kV/m
+field_dict['E_RF_kVm'] = 4  # kV/m
 
 field_dict = define_default_field(settings, field_dict=field_dict)
 
@@ -44,6 +43,7 @@ save_dir = ''
 save_dir += 'tmax_' + str(settings['sim_cyclotron_periods'])
 save_dir += '_B0_' + str(field_dict['B0'])
 save_dir += '_T_' + str(settings['T_keV'])
+save_dir += '_nonMB_'
 save_dir += '_' + str(field_dict['RF_type'])
 save_dir += '_ERF_' + str(field_dict['E_RF_kVm'])
 save_dir += '_alpha_' + '_'.join([str(alpha_detune) for alpha_detune in field_dict['alpha_detune_list']])
@@ -65,7 +65,8 @@ total_number_of_combinations = 2000
 
 # sampling velocity from Maxwell-Boltzmann
 scale = np.sqrt(settings['kB_eV'] * settings['T_eV'] / settings['mi'])
-v_abs_samples = maxwell.rvs(size=total_number_of_combinations, scale=scale)
+# v_abs_samples = maxwell.rvs(size=total_number_of_combinations, scale=scale)
+v_abs_samples = settings['v_th'] * np.ones(total_number_of_combinations)  # testing constant velocity
 
 # sampling a random direction
 rand_unit_vec = np.random.randn(total_number_of_combinations, 3)
